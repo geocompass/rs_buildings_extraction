5151efe5bba7ec50abb82e60592261bc49b820ff
wucangeo <wucangeo@gmail.com>
2019-10-18 11:00:49 +0800

    添加打开日志接口

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint
-from app.api.v1 import test, predict_buildings, buia, train, predict, wmts, geojson
+from app.api.v1 import test, predict_buildings, buia, train, predict, wmts, geojson, tools
 
 
 def create_blueprint_v1():
@@ -13,5 +13,6 @@ def create_blueprint_v1():
     train.api.register(bp_v1)
     wmts.api.register(bp_v1)
     geojson.api.register(bp_v1)
+    tools.api.register(bp_v1)
 
     return bp_v1
